1c0efb436a233b2f5fbbb1a6a9added342f30d98
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-01-08 14:55:36 -0500

    Starting a state model for the campaign manager

---
+++ src/radical/cm/utils/states.py
@@ -0,0 +1,19 @@
+"""
+Author: Ioannis Paraskevakos
+License: MIT
+Copyright: 2018-2019
+"""
+
+
+# ------------------------------------------------------------------------------
+#  States
+
+NEW = 0  # New campaign is submitted
+PLANNING = 1 # Planning the exeuction of the campaign
+EXECUTING = 2 # At least one workflow is executing
+DONE = 3 # Campaign has finished successfully
+FAILED = 4 # Campaign execution has failed
+CANCELED = 5 3 # Campaign got canceled by the user.
+CFINAL = [DONE, FAILED, CANCELED] # Final states for a campaign.
+
+# ------------------------------------------------------------------------------
