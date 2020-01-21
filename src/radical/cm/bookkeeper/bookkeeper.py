12a4da9fbf3e507758b398548e02897b604187df
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-01-20 21:10:09 -0500

    Work on the bookkeeper. Issue #9

--- src/radical/cm/bookkeeper/bookkeeper.py
+++ src/radical/cm/bookkeeper/bookkeeper.py
@@ -6,6 +6,8 @@ Copyright: 2018-2019
 
 import radical.utils as ru
 from ..planner import HeftPlanner, RandomPlanner
+from ..utils import states as st
+from ..enactor import EmulatedEnactor
 
 class Bookkeeper(object):
 
@@ -22,15 +24,28 @@ class Bookkeeper(object):
 
     def __init__(self, campaign, resources, objective=None, planner='random'):
 
-        self._campaign = campaign
+        self._campaign = {'campaign': campaign,
+                          'state': st.NEW
+                         }
         self._resources = resources
+        self._checkpoints = None
+        self._plan = None
+        self._unavail_resources = []
+        self._execution_status = dict()
+        self._time = 0 # The time in the campaign's world.
+        self._workflows_to_monitor = list()
+        self._enactor = EmulatedEnactor()
+
         self._logger = ru.Logger(name='radical.cm.bookkeeper', level='DEBUG')
+        self._prof   = ru.Profiler(name='radical.cm.bookkeeper',
+                                   path=os.getcwd() + '/')
+        
         if planner.lower() == 'random':
             self._planner = RandomPlanner(campaign=self._campaign,
                                           resources=self._resources,
                                           num_oper=[])  # TODO: fix num_oper
         elif planner.lower() == 'heft':
-            self._planner = HeftPlanner((campaign=self._campaign,
+            self._planner = HeftPlanner(campaign=self._campaign,
                                           resources=self._resources,
                                           num_oper=[])  # TODO: fix num_oper
         else:
@@ -39,18 +54,104 @@ class Bookkeeper(object):
             self._planner = RandomPlanner(campaign=self._campaign,
                                           resources=self._resources,
                                           num_oper=[])  # TODO: fix num_oper
-        self._enactor = None  # TODO: I need an enactor connected through a cb?
 
-    def execute(self):
-        '''
-        This is the main method. It calls the planner, sends workflows to the
-        enactor and keeps track of the execution.
-        '''
-
-        cur_plan = self._planner.plan()
-
-        ## TODO: Verify plan
-
-        for (workflow, resource) in plan:
-            self._enector.enact(workflow, resource)
+        # Populate the execution status dictionaty with workflows
+        for workflow in self._campaign:
+            self._execution_status[workflow['id']] = st.NEW
         
+    def _update_checkpoints(self):
+        '''
+        Create a list of timestamps when workflows may start executing or end.
+        TODO: this method does not take into account dynamic resources where
+              delays in execution affect the makespan.
+        '''
+
+        self._checkpoints = [0]
+
+        for work in self._plan:
+            if work[3] not in self._checkpoints:
+                self._checkpoints.append(work[3])
+        
+        self._checkpoints.sort()
+
+
+    def _verify_objective(self):
+        '''
+        This private method verifies the objective. It takes as input a plan,
+        and an objective, calculates when the campaign will actually finish and
+        returns True or False.
+
+        *Parameters*
+        plan: a plan e.g.: plan = [('W1', 523, 0, 102.5793499043977),
+                                   ('W9', 487, 0, 82.13552361396304),
+                                   ('W3', 487, 82.13552361396304, 147.22792607802876),
+                                   ('W5', 523, 102.5793499043977, 140.82026768642447),
+                                   ('W10', 96, 0, 166.66666666666666),
+                                   ('W4', 523, 140.82026768642447, 166.0),
+                                   ('W2', 487, 147.22792607802876, 170.22792607802876),
+                                   ('W7', 523, 166.0, 185.11854684512429),
+                                   ('W8', 487, 170.22792607802876, 180.54620123203287),
+                                   ('W6', 96, 166.66666666666666, 179.16666666666666)]
+        objective: when the campaign should finish in terms of time.
+
+        TODO: Currently the objective is given as a number in some unit of time.
+              The objective can be in a more general representation.
+        '''
+
+        # Update the checkpoints based on the current state of the execution and
+        # the plan
+
+        if self._checkpoints[-1] > self._objective:
+            return False
+        else:
+            return True
+
+
+    def work(self):
+        
+        if self._plan is None:
+            self._plan = self._planner.plan(campaign=self._campaign,
+                                      reources=self._resources,
+                                      num_oper=[something])
+
+        self._update_checkpoints()
+        
+        ## TODO: Verify plan
+        if self._verify_objective():
+            workflows = list()  # Workflows to enact
+            resource = list()  # The selected resources
+            for (workflow, resource, start_time, _) in self._plan:
+                if start_time == self._time:
+                    workflows.append(workflow)
+                    resources.append(resources)
+            self._enactor.enact(workflows=workflows, resources=resources)
+            self._workflows_to_monitor += workflows
+            self._unavail_resources += resources
+                    
+        else:
+            raise RuntimeError("Objective cannot be satisfied. Ending execution")
+
+    
+    def monitor(self):
+        '''
+        This method monitors the state of the workflows. If the state is one of
+        the final states, it removes the workflow from the monitoring list, and
+        releases the resource. Otherwise if appends it to the end.
+        '''
+        
+        while self._workflows_to_monitor:
+            workflow = self._workflows_to_monitor.pop(0)
+            resource = self._unavail_resources.pop(0)
+            if self._execution_status[workflow['id']]['state'] not in st.FINAL:
+                self._workflows_to_monitor.append(workflow)
+                self._unavail_resources.append(resource)
+
+    def get_makespan(self):
+        '''
+        Returns the makespan of the campaign based on the current state of
+        execution
+        '''
+
+        self._update_checkpoints()
+
+        return self._checkpoints[-1]
