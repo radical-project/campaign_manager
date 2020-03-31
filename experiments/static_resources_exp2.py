01cce3023f95585ef2a92a8b4490e950b99fb2ad
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-03-31 11:46:32 -0400

    New plots

--- experiments/static_resources_exp2.py
+++ experiments/static_resources_exp2.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
                  {'id': 3, 'performance': 1},
                  {'id': 4, 'performance': 1}]
     campaign_sizes = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    results = pd.DataFrame(columns=['size','planner','plan','makespan'])
     for cm_size in campaign_sizes:
         print('Current campaign size: %d' % cm_size)
         campaign, num_oper = campaign_creator(num_workflows=cm_size, heterogeneity=True)
